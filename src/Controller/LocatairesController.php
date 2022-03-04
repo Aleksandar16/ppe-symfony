370ee978a3c66a33cf833d9ed14271bcf8accbaa
shakeeljeerooburkhan <shakeel.jeerooburkhan995@gmail.com>
2022-03-04 16:48:55 +0100

    Liste des locataires fonctionnels
    Manque affichage uniquement des tenant

--- src/Controller/LocatairesController.php
+++ src/Controller/LocatairesController.php
@@ -4,9 +4,8 @@ namespace App\Controller;
 
 // use App\Entity\User;
 // use App\Entity\Rent;
-// use App\Repository\RentRepository;
-
 use App\Repository\UserRepository;
+use App\Repository\RentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -14,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class LocatairesController extends AbstractController
 {
-    public function index(RentRepository $rentRepository): Response
+    public function index(RentRepository $rentRepository,UserRepository $userRepository): Response
     {
         return $this->render('locataires/index.html.twig', [
             'locataires' => $userRepository->findBy(array(), array('id' => 'DESC') ,array('roles' => 'ROLE_TENANT')),
